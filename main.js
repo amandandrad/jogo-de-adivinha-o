ca68b961c5f4b148e191f998542036ae96336052
amandandrad <andradeamanda2508@gmail.com>
2024-05-28 20:58:41 -0300

    pequenas correções

--- main.js
+++ main.js
@@ -18,38 +18,35 @@ function handleClickEnter(e) {
   }
 }
 
-// função callback atribuida ao botão (só funciona com o click)
+// função callback atribuida ao botão
 function handleTryClick(event) {
   event.preventDefault();
   const inputNumber = document.querySelector("#inputNumber");
-
   if (inputNumber.value !== "") {
     if (inputNumber.value >= 0 && inputNumber.value <= 1000) {
       xAttempts++;
-      if (Number(inputNumber.value) == randomNumber) {
+      if (Number(inputNumber.value) > randomNumber) {
+        alert("Eita, o número é menor. Tente novamente!");
+      } else if (Number(inputNumber.value) < randomNumber) {
+        alert("Vish, o número é maior. Tente novamente!");
+      } else {
         toggleScreen();
         screen2.querySelector(
           "h2"
-        ).innerText = `Acertou em ${xAttempts} tentativas.`;
-      } else if (Number(inputNumber.value > randomNumber)) {
-        alert("Eita, o número é menor. Tente novamente!");
-      } else {
-        alert("Vish, o número é maior. Tente novamente!");
+        ).innerText = ` Acertou em ${xAttempts} tentativas.`;
       }
     } else {
-      inputNumber.value = "";
       alert("Por favor, insira um número entre 0 e 1000.");
     }
   }
-
   inputNumber.value = "";
 }
 
 // função callback irá iniciar o programa novamente
 function handleResetClick() {
   toggleScreen();
-  xAttempts = 1;
-  randomNumber = Math.round(Math.random() * 10);
+  xAttempts = 0;
+  randomNumber = Math.round(Math.random() * 1000);
 }
 
 //função para simplificar a troca do hide
