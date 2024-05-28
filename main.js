21839a2eb53df749026b15ff0cddd30043dd9d4c
amandandrad <andradeamanda2508@gmail.com>
2024-05-27 23:28:05 -0300

    adicionando dificuldade e dicas

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //Variáveis
-let randomNumber = Math.round(Math.random() * 10); // criação de variavel para gerar número aleatório
+let randomNumber = Math.round(Math.random() * 1000);
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
 const btnTry = document.querySelector("#btnTry");
 const btnReset = document.querySelector("#btnReset");
-let xAttempts = 0; //variavél para armazenar número de tentativas
+let xAttempts = 0;
 
 //Eventos
 btnTry.addEventListener("click", handleTryClick);
@@ -20,28 +20,28 @@ function handleClickEnter(e) {
 
 // função callback atribuida ao botão (só funciona com o click)
 function handleTryClick(event) {
-  // linha 11 é: não faça o padrão! que seria enviar o form
   event.preventDefault();
-
-  // seleciona o botão através do ID do css
   const inputNumber = document.querySelector("#inputNumber");
-  if (inputNumber.value >= 0 && inputNumber.value <= 10) {
-    xAttempts++; // inclemeta +1 a cada número válido digitado
-    console.log("Tentativa:", xAttempts);
-    if (Number(inputNumber.value) == randomNumber) {
-      /* se o número digitado pelo usuário = número aleatório então no screen 1 vai ser adicionado a class hide (que tem o display: none, onde vai deixar a tela branca) e remove a hide do screen2 para que ele apareça */
-      toggleScreen();
 
-      screen2.querySelector(
-        "h2"
-      ).innerText = `Acertou em ${xAttempts} tentativas.`;
+  if (inputNumber.value !== "") {
+    if (inputNumber.value >= 0 && inputNumber.value <= 1000) {
+      xAttempts++;
+      if (Number(inputNumber.value) == randomNumber) {
+        toggleScreen();
+        screen2.querySelector(
+          "h2"
+        ).innerText = `Acertou em ${xAttempts} tentativas.`;
+      } else if (Number(inputNumber.value > randomNumber)) {
+        alert("Eita, o número é menor. Tente novamente!");
+      } else {
+        alert("Vish, o número é maior. Tente novamente!");
+      }
+    } else {
+      inputNumber.value = "";
+      alert("Por favor, insira um número entre 0 e 1000.");
     }
-  } else {
-    inputNumber.value = "";
-    alert("Por favor, insira um número entre 0 e 10.");
   }
 
-  //após o clique no botão tentar, automaticamente o campo fica limpo
   inputNumber.value = "";
 }
 
